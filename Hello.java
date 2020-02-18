b2834fb0b97801ebbab46e1abf127f0de06f5335
sizwe.ncikana@gmail.com <japhetmibatu1@gmail.com>
2020-02-18 15:20:40 +0200

    requested user to add name

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
+import java.util.*;
 public class Hello{
+    Scanner Scanner = new Scanner (System.in);
     public static void main (String [] args){
         System.out.println("Pilisa Gqokoma");
+
+        System.out.println("Enter your nickname");
+        String name = Scanner.next();
     }
 }
